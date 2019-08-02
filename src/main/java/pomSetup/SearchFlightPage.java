12f32a94d8261aff51955f8863109bb5f0b462eb
siddharthamukherjeeqa <siddharthamukherjeeqa@gmail.com>
2019-08-03 00:27:12 +0530

    Commit message

--- src/main/java/pomSetup/SearchFlightPage.java
+++ src/main/java/pomSetup/SearchFlightPage.java
@@ -17,6 +17,7 @@ public class SearchFlightPage {
 	public void intialize() {
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://www.expedia.com/");
 		etSearch = new ExpediaTravelSearch(driver);
@@ -24,10 +25,16 @@ public class SearchFlightPage {
 	@Test
 	public void searchFlight() {
 		etSearch.clickFlightsTab();
+		System.out.println("1");
 		etSearch.enterOriginDetails("Hyderabad");
+		System.out.println("2");
 		etSearch.enterdestinationDetails("New York");
+		System.out.println("3");
 		etSearch.enterStartDate("01/01/2020");
+		System.out.println("4");
 		etSearch.enterEndDate("01/01/2020");
+		System.out.println("5");
 		etSearch.searchFlight();
+		System.out.println("6");
 	}
 }
